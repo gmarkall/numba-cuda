30fdb6d80aa56980a6924487ca7c10791533c3c0
Graham Markall <gmarkall@nvidia.com>
2025-03-25 11:32:08 +0000

    Fixes to enable CUDASIM test suite to run again

--- numba_cuda/numba/cuda/simulator/__init__.py
+++ numba_cuda/numba/cuda/simulator/__init__.py
@@ -29,6 +29,7 @@ if config.ENABLE_CUDASIM:
     sys.modules['numba.cuda.cudadrv.devicearray'] = cudadrv.devicearray
     sys.modules['numba.cuda.cudadrv.devices'] = cudadrv.devices
     sys.modules['numba.cuda.cudadrv.driver'] = cudadrv.driver
+    sys.modules['numba.cuda.cudadrv.linkable_code'] = cudadrv.linkable_code
     sys.modules['numba.cuda.cudadrv.runtime'] = cudadrv.runtime
     sys.modules['numba.cuda.cudadrv.drvapi'] = cudadrv.drvapi
     sys.modules['numba.cuda.cudadrv.error'] = cudadrv.error
@@ -36,3 +37,6 @@ if config.ENABLE_CUDASIM:
 
     from . import compiler
     sys.modules['numba.cuda.compiler'] = compiler
+
+    from numba.cuda.simulator import runtime as _runtime
+    sys.modules['numba.cuda.runtime'] = _runtime
---
+++ numba_cuda/numba/cuda/simulator/runtime/__init__.py
@@ -0,0 +1,1 @@
+rtsys = None
