ffac0cc67534cf1e0a53d2122e14a2cb43e30bc0
Graham Markall <gmarkall@nvidia.com>
2025-10-22 17:16:05 +0100

    Add datamodel redirectors

---
+++ numba_cuda/numba/cuda/datamodel/packer.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-2-Clause
+
+import importlib
+import sys
+from numba.cuda.utils import _RedirectSubpackage
+
+if importlib.util.find_spec("numba"):
+    sys.modules[__name__] = _RedirectSubpackage(
+        locals(), "numba.core.datamodel.packer"
+    )
+else:
+    sys.modules[__name__] = _RedirectSubpackage(
+        locals(), "numba.cuda.datamodel.packer"
+    )
